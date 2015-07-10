892344bce099b9b3d9fb4b9d9348bb430fef6ea2
Paul Borchert <paul.borchert@gbv.de>
2015-07-10 10:11:59 +0000

    add appbaseurl support

--- webpages/editor/editor-doctype.js
+++ webpages/editor/editor-doctype.js
@@ -18,25 +18,39 @@ function changeHostOptions(){
 
         
 function createGenreOptions() {
-  var Options="";
-  $(GenreXML).find('categories > category').each(function(){
-    var Title = $(this).children('label[xml\\:lang="de"]').attr('text');
-    var id = $(this).attr('ID');
-    Options+='<option value="'+id+'">' + Title + '</option>';
-    $(this).find('category').each(function(){
-      var Title = $(this).children('label[xml\\:lang="de"]').attr('text');
-      var id = $(this).attr('ID');
-      Options+='<option value="'+id+'">&nbsp;&nbsp;&nbsp;' + Title + '</option>';
-    });
-  });
-  $('#genre').append(Options);
+	var Options="";
+  	$(GenreXML).find('categories > category').each(function(){
+		var Title = $(this).children('label[xml\\:lang="de"]').attr('text');
+		var id = $(this).attr('ID');
+		Options+='<option value="'+id+'">' + Title + '</option>';
+		$(this).find('category').each(function(){
+	  		var Title = $(this).children('label[xml\\:lang="de"]').attr('text');
+	  		var id = $(this).attr('ID');
+			Options+='<option value="'+id+'">&nbsp;&nbsp;&nbsp;' + Title + '</option>';
+	  	});
+	});
+	$('#genre').append(Options);
+}
+
+function toggleMoreOptions( duration ) {
+	if ( $('#more_options_box').is(':visible') ) {
+		$('#more_options_box').fadeOut( duration );
+		$('#more_options_label').html('weitere Optionen einblenden');
+		$('#more_options_button').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+		localStorage.setItem("moreOptions", "closed");
+	} else {
+		$('#more_options_box').fadeIn( duration );
+		$('#more_options_label').html('weitere Optionen ausblenden');
+		$('#more_options_button').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+		localStorage.setItem("moreOptions", "opened");
+	}
 }
   
 $(document).ready( function() {
   $('#genre').change(function (){changeHostOptions();});
   $.ajax({
     method: "GET",
-    url: "http://reposis-test.gbv.de/hagen/api/v1/classifications/mir_genres",
+    url: webApplicationBaseURL+"api/v1/classifications/mir_genres",
     dataType: "xml"
   }) .done(function( xml ) {
     GenreXML=xml;
