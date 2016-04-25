88f4fa8e961db99941597c328497e98da53fad7d
Paul Borchert <paul.borchert@gbv.de>
2016-04-25 20:19:22 +0000

    temp oas

--- resources/xsl/metadata/mods-metadata-page.xsl
+++ resources/xsl/metadata/mods-metadata-page.xsl
@@ -191,6 +191,7 @@
                 <xsl:value-of select="$statistics/report/entries/entry/access/type" />
                 <!-- <xsl:value-of select="$statistics/report/entries/entry/access/type[text()='counter']/../count"/> -->
                 <!-- End: OAS -->
+                <div data-oasproviderurl="https://ub-deposit.fernuni-hagen.de/graphprovider/"/>
               </div>
             </div>
           </xsl:if>
---
+++ save/webpages/js/oas.js
@@ -0,0 +1,34 @@
+//Class OA Widget
+
+function OAWidget (elememt,providerurl) {
+  this.providerurl = providerurl;
+  this.$element = $(element);
+}
+
+OAWidget.prototype= {
+  constructor: OAWidget
+  
+  ,getData: function () {
+    $.ajax({
+		method : "GET",
+		url : this.providerurl + "jsonloader.php?identifier=oai:ub-deposit.fernuni-hagen.de:mir_mods_00000008&amp;from=2016-01-01&amp;until=2016-03-31&amp;formatExtension=xml&amp;granularity=total",
+		dataType : "xml"
+	}).done(function(data) {
+		render(this.$element, data)
+	}).error(function(e) {
+		console.log("Fehler beim Holen der Daten vom Graphprovider");
+	});
+  }
+  
+  ,render: function () {
+    console.log("Rendern");
+  }
+}
+
+$﻿(document).ready(function() {
+  $("[data-oasproviderurl != ''] ").each(function(index, oasdiv) {
+    $(oasdiv).text("each test");
+    
+  }
+
+});
