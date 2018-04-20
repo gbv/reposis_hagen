495efe7018c056a27fd236ad50c0762d7ac74601
Paul Borchert <paul.borchert@gbv.de>
2018-04-20 10:09:58 +0000

    use complete search site to get results for startpage

--- save/webpages/content/index.js
+++ save/webpages/content/index.js
@@ -2,10 +2,11 @@
 $(document).ready( function() {
   $.ajax({
     method: "GET",
-    url: webApplicationBaseURL+"servlets/solr/find?qry=objectType:mods&rows=3&sort=created+desc&XSL.Transformer=response-resultlist",
+    url: webApplicationBaseURL+"servlets/solr/find?qry=objectType:mods&rows=3&sort=created+desc",
     dataType: "html"
   }) .done(function( html ) {
-    $('#hit_list').html(html);
+    var hitListHtml=$(html).find('#hit_list').html();
+    $('#hit_list').html(hitListHtml);
     $('.hit_counter').html("&nbsp");
     $('.hit_options').html("&nbsp");
   });
