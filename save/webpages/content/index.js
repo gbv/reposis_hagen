531f4ce458fec54684fb09d88107ef8650fe9cee
Paul Borchert <paul.borchert@gbv.de>
2022-01-07 14:49:24 +0100

    fix solr4- solr7 issues use select instead of find

--- save/webpages/content/index.js
+++ save/webpages/content/index.js
@@ -2,7 +2,7 @@
 $(document).ready( function() {
   $.ajax({
     method: "GET",
-    url: webApplicationBaseURL+"servlets/solr/find?qry=objectType:mods&rows=3&sort=created+desc",
+    url: webApplicationBaseURL+"servlets/solr/select?q=objectType:mods  AND state:published&rows=3&sort=created+desc",
     dataType: "html"
   }) .done(function( html ) {
     var hitListHtml=$(html).find('#hit_list').html();
