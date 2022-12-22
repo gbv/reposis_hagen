a931b488af2f990ef2dae39a0f7967a63ba1d04a
Paul Borchert <paul.borchert@gbv.de>
2022-12-22 17:26:22 +0100

    remove elements from hitlist on startpage

---
+++ src/main/resources/META-INF/resources/content/index.js
@@ -0,0 +1,9 @@
+           
+$(document).ready( function() {
+  
+    $('.hit_counter').html("&nbsp");
+    $('.hit_options').html("&nbsp");
+    $('.single_hit_option').html("&nbsp");
+  
+});
+			
