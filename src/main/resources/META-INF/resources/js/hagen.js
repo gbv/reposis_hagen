519ededfd82f557e7bfded454fea29ac111cd700
Lennard Golsch <lennard.golsch@icloud.com>
2026-08-05 09:03:53 +0200

    feat: introduce hagen.js

---
+++ src/main/resources/META-INF/resources/js/hagen.js
@@ -0,0 +1,15 @@
+function replaceMaskedEmails() {
+  document.querySelectorAll('span.madress').forEach(span => {
+    const address = span.textContent.replace(' [at] ', '@');
+    const link = document.createElement('a');
+    link.href = `mailto:${address}`;
+    link.textContent = address;
+    span.replaceWith(link);
+  });
+}
+
+function init() {
+  replaceMaskedEmails();
+}
+
+document.addEventListener("DOMContentLoaded", init);
